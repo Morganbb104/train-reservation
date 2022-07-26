85e6541a0feccd085f83937d521121c23a6ba0ec
scouring pad <hcc780921@gmail.com>
2022-07-26 08:12:37 -0700

    first commit

---
+++ Chengchien-Huang-train-reservation/src/main/java/com/example/ChengchienHuangtrainreservation/ChengchienHuangTrainReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChengchienHuangtrainreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChengchienHuangTrainReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChengchienHuangTrainReservationApplication.class, args);
+	}
+
+}
